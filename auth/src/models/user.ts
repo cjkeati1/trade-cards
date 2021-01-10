adb22e7e3eb4695c8d32144eef627acc25c907c3
Connor Keating <cjkeati1@asu.edu>
2021-01-09 20:21:22 -0700

    Created a User model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose';
 
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
