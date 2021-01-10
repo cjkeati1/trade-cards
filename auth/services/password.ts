d924d58f7154538d8aefe3dd1f0e8865ecd098c9
Connor Keating <cjkeati1@asu.edu>
2021-01-09 22:51:28 -0700

    implemented password hashing

---
+++ auth/services/password.ts
@@ -0,0 +1,24 @@
+import {scrypt, randomBytes} from "crypto";
+import {promisify} from "util";
+
+const scryptAsync = promisify(scrypt);
+
+const getPasswordBuffer = async (password: string, salt: string) => {
+    return (await scryptAsync(password, salt, 64)) as Buffer;
+};
+
+export class Password {
+    static toHash = async (password: string) => {
+        const salt = randomBytes(8).toString('hex');
+        const buf = await getPasswordBuffer(password, salt);
+
+        return `${buf.toString('hex')}.${salt}`;
+    };
+
+    static compare = async (storedPassword: string, suppliedPassword: string) => {
+        const [hashedPassword, salt] = storedPassword.split('.');
+        const buf = await getPasswordBuffer(suppliedPassword, salt);
+
+        return buf.toString('hex') === hashedPassword;
+    };
+}
