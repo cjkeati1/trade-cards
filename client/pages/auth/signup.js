df3c8cb7fce095d6b1418e5c4628a396a0f048e3
Connor Keating <cjkeati1@asu.edu>
2021-01-11 00:39:39 -0700

    created use request hook. done with signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,11 +1,27 @@
 import {useState} from 'react';
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 
 const SignUp = () => {
    const [email, setEmail] = useState('');
    const [password, setPassword] = useState('');
+   const {doRequest, errors} = useRequest({
+      url: '/api/users/signup',
+      method: 'post',
+      body: {
+         email,
+         password
+      },
+      onSuccess: () => Router.push('/')
+   });
+
+   const onSubmit = async event => {
+      event.preventDefault();
+      await doRequest();
+   }
 
    return (
-      <form>
+      <form onSubmit={onSubmit}>
          <h1>Sign Up</h1>
          <div className="form-group">
             <label>Email Address</label>
@@ -25,6 +41,7 @@ const SignUp = () => {
             />
          </div>
          <button className="btn btn-primary">Sign Up</button>
+         {errors}
       </form>
    );
 };
