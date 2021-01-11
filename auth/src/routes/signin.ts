9f56036ee2ca16292c85bb2c6ea6f1f3627a2207
Connor Keating <cjkeati1@asu.edu>
2021-01-10 17:30:57 -0700

    Started signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,35 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body, validationResult} from "express-validator";
+import jwt from 'jsonwebtoken';
+
+import {User} from "../models/user";
+import {RequestValidationError} from "../errors/request-validation-error";
+import {BadRequestError} from "../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('signed in')
+router.post('/api/users/signin', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must supply a password')
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+
+    const {email} = req.body;
+
+    const existingUser = await User.findOne({email});
+
+    if (!existingUser) {
+        throw new BadRequestError('A user with that email does not exist');
+    }
 });
 
 export {router as signinRouter};
