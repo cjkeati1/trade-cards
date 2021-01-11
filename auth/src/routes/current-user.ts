5ed72272a5af9743985df08d414fcdd73f9e88c6
Connor Keating <cjkeati1@asu.edu>
2021-01-10 18:49:06 -0700

    Created middleware for protected routes

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import jwt from "jsonwebtoken";
 import {currentUser} from "../middleware/current-user";
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-    // @ts-ignore
-    console.log(req.currentUser!.iat);
     return res.send({currentUser: req.currentUser || null});
 });
 
