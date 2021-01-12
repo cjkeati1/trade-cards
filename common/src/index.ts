d691b413a8e88c9f632cffecdd50af5066931d6d
Connor Keating <cjkeati1@asu.edu>
2021-01-11 21:45:14 -0700

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,5 @@
-export * from './errors/bad-request-error'
+// Re-export stuff from errors and middlewares
+export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
@@ -8,4 +9,4 @@ export * from './errors/request-validation-error';
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
-export * from './middlewares/validate-request';
+export * from './middlewares/validate-request'
