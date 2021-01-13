5ae49fca1c5f941760231cff7a60711c35e1936c
Connor Keating <cjkeati1@asu.edu>
2021-01-13 00:51:29 -0700

    minor change to new card test

--- cards/src/routes/__test__/new.test.ts
+++ cards/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import {app} from '../../app';
 import {CardCondition} from "@ckcards/common/build";
+import {Card} from "../../models/card";
 
 it('has a route handler listening to /api/cards for post requests', async () => {
     const res = await request(app)
@@ -78,16 +79,27 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a card with valid inputs', async () => {
-    // add in a check to make sure a card was saved
+    const title = 'Borrelsword Dragon';
+    const condition = CardCondition.Mint;
+    const description = 'Maximum Gold - Singles';
+    const price = 4;
+
     await request(app)
         .post('/api/cards')
         .set('Cookie', global.getAuthCookie())
         .send({
-            title: 'Title',
-            condition: CardCondition.Excellent,
-            description: 'Description',
-            price: 20
+            title,
+            condition,
+            description,
+            price
         })
         .expect(201);
+
+    const cards = await Card.find({});
+    expect(cards.length).toEqual(1);
+    expect(cards[0].price).toEqual(price);
+    expect(cards[0].title).toEqual(title);
+    expect(cards[0].condition).toEqual(condition);
+    expect(cards[0].description).toEqual(description);
 
 });
