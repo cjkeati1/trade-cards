83e47128e0b0a79abba10406a48064212ce81a39
Connor Keating <cjkeati1@asu.edu>
2021-01-12 21:58:13 -0700

    Created enum for card condition in common module

---
+++ common/src/types/card-condition.ts
@@ -0,0 +1,9 @@
+export enum CardCondition {
+    Mint = 'mint',
+    NearMint = 'near-mint',
+    Excellent = 'excellent',
+    Good = 'good',
+    LightPlayed = 'light-played',
+    Played = 'played',
+    Poor = 'poor'
+}
